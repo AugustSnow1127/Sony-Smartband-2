1b068bb30e315f0d4e859141f0844ed86d7fdad2
Angus Zhang <angusqweas@gmail.com>
2019-01-29 12:26:33 +0800

    Create MainActivity.java
    Including Sensor API, Recording API, Ble API
    not yet testing, maybe wrong

---
+++ MainActivity.java
@@ -0,0 +1,393 @@
+package com.example.angus.goooglefit1;
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessStatusCodes;
+import com.google.android.gms.fitness.data.BleDevice;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Subscription;
+import com.google.android.gms.fitness.data.Value;
+import com.google.android.gms.fitness.request.BleScanCallback;
+import com.google.android.gms.fitness.request.DataSourcesRequest;
+import com.google.android.gms.fitness.request.OnDataPointListener;
+import com.google.android.gms.fitness.request.SensorRequest;
+import com.google.android.gms.fitness.request.StartBleScanRequest;
+import com.google.android.gms.fitness.result.DataSourcesResult;
+import com.google.android.gms.fitness.result.ListSubscriptionsResult;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class MainActivity extends AppCompatActivity implements OnDataPointListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
+
+    //Recording Api Part
+    private Button mCancelSubscriptionsBtn;
+    private Button mShowSubscriptionsBtn;
+    private ResultCallback<Status> mSubscribeResultCallback;
+    private ResultCallback<Status> mCancelSubscriptionResultCallback;
+    private ResultCallback<ListSubscriptionsResult> mListSubscriptionsResultCallback;
+    private GoogleApiClient mRecordingApiClient;
+
+    //Sensor Api Part
+    private static final int REQUEST_OAUTH = 1;
+    private static final String AUTH_PENDING = "auth_state_pending";
+    private boolean authInProgress = false;
+    private GoogleApiClient mApiClient;
+
+    //Ble Api Part
+    private static final int REQUEST_BLUETOOTH = 1001;
+
+    private ResultCallback mResultCallback = new ResultCallback() {
+        @Override
+        public void onResult(@NonNull Result result) {
+            Status status = result.getStatus();
+            if (!status.isSuccess()) {
+                switch (status.getStatusCode()) {
+                    case FitnessStatusCodes.DISABLED_BLUETOOTH:
+                        try {
+                            status.startResolutionForResult(
+                                    MainActivity.this, REQUEST_BLUETOOTH);
+                        } catch (IntentSender.SendIntentException e) {
+
+                        }
+                        break;
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Recording Api Part
+        initViews();
+        initCallbacks();
+
+
+
+        if (savedInstanceState != null) {
+            authInProgress = savedInstanceState.getBoolean(AUTH_PENDING);
+        }
+
+        //Sensor Api Part
+        mApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Fitness.SENSORS_API)
+                .addApi(Fitness.BLE_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        //Recording Api Part
+        mRecordingApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Fitness.RECORDING_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ))
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, 0, this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Fitness.SensorsApi.remove( mApiClient, this )
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        if (status.isSuccess()) {
+                            mApiClient.disconnect();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(AUTH_PENDING, authInProgress);
+    }
+
+    private void registerFitnessDataListener(DataSource dataSource, DataType dataType) {
+
+        SensorRequest request = new SensorRequest.Builder()
+                .setDataSource( dataSource )
+                .setDataType( dataType )
+                .setSamplingRate( 1, TimeUnit.SECONDS )
+                .build();
+
+        Fitness.SensorsApi.add(mApiClient, request, this)
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        if (status.isSuccess()) {
+                            Log.e("GoogleFit", "SensorApi successfully added");
+                        } else {
+                            Log.e("GoogleFit", "adding status: " + status.getStatusMessage());
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        //startBleScan
+        startBleScan ();
+
+        DataSourcesRequest dataSourceRequest = new DataSourcesRequest.Builder()
+                .setDataTypes( DataType.TYPE_STEP_COUNT_CUMULATIVE )
+                .setDataSourceTypes( DataSource.TYPE_RAW )
+                .build();
+
+        ResultCallback<DataSourcesResult> dataSourcesResultCallback = new ResultCallback<DataSourcesResult>() {
+            @Override
+            public void onResult(DataSourcesResult dataSourcesResult) {
+                for( DataSource dataSource : dataSourcesResult.getDataSources() ) {
+                    if( DataType.TYPE_STEP_COUNT_CUMULATIVE.equals( dataSource.getDataType() ) ) {
+                        registerFitnessDataListener(dataSource, DataType.TYPE_STEP_COUNT_CUMULATIVE);
+                    }
+                }
+            }
+        };
+
+        Fitness.SensorsApi.findDataSources(mApiClient, dataSourceRequest)
+                .setResultCallback(dataSourcesResultCallback);
+
+        //Recording Api Part
+        Fitness.RecordingApi.subscribe(mRecordingApiClient, DataType.TYPE_STEP_COUNT_DELTA)
+                .setResultCallback(mSubscribeResultCallback);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if( !authInProgress ) {
+            try {
+                authInProgress = true;
+                connectionResult.startResolutionForResult( MainActivity.this, REQUEST_OAUTH );
+            } catch(IntentSender.SendIntentException e ) {
+                Log.e( "GoogleFit", "sendingIntentException " + e.getMessage() );
+            }
+        } else {
+            Log.e( "GoogleFit", "authInProgress" );
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_BLUETOOTH:
+                startBleScan();
+                Log.e( "onActivityResult", "REQUEST_BLUETOOTH" );
+                break;
+            case REQUEST_OAUTH:
+                authInProgress = false;
+                if( resultCode == RESULT_OK ) {
+                    if( !mApiClient.isConnecting() && !mApiClient.isConnected() ) {
+                        mApiClient.connect();
+                    }
+                } else if( resultCode == RESULT_CANCELED ) {
+                    Log.e( "onActivityResult", "RESULT_CANCELED" );
+                }
+                break;
+            default:
+                Log.e("onActivityResult", "problem");
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e("RecordingAPI", "onConnectionSuspended");
+
+    }
+
+    @Override
+    public void onDataPoint(DataPoint dataPoint) {
+        for( final Field field : dataPoint.getDataType().getFields() ) {
+            final Value value = dataPoint.getValue( field );
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), "Field: " + field.getName() + " Value: " + value, Toast.LENGTH_SHORT).show();
+                    ((TextView)findViewById(R.id.textView)).setText(value.asString());
+                }
+            });
+        }
+    }
+
+    //Recording Api Part
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btn_cancel_subscriptions: {
+                cancelSubscriptions();
+                break;
+            }
+            case R.id.btn_show_subscriptions: {
+                showSubscriptions();
+                break;
+            }
+        }
+    }
+
+    //Recording Api Part
+    private void showSubscriptions() {
+        Fitness.RecordingApi.listSubscriptions(mRecordingApiClient)
+                .setResultCallback(mListSubscriptionsResultCallback);
+    }
+
+    //Recording Api Part
+    private void cancelSubscriptions() {
+        Fitness.RecordingApi.unsubscribe(mRecordingApiClient, DataType.TYPE_STEP_COUNT_DELTA)
+                .setResultCallback(mCancelSubscriptionResultCallback);
+    }
+
+
+    //Recording Api Part
+    private void initViews() {
+        mCancelSubscriptionsBtn = (Button) findViewById(R.id.btn_cancel_subscriptions);
+        mShowSubscriptionsBtn = (Button) findViewById(R.id.btn_show_subscriptions);
+
+        mCancelSubscriptionsBtn.setOnClickListener(this);
+        mShowSubscriptionsBtn.setOnClickListener(this);
+    }
+
+    //Recording Api Part
+    private void initCallbacks() {
+        mSubscribeResultCallback = new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if (status.isSuccess()) {
+                    if (status.getStatusCode() == FitnessStatusCodes.SUCCESS_ALREADY_SUBSCRIBED) {
+                        Log.e( "RecordingAPI", "Already subscribed to the Recording API");
+                    } else {
+                        Log.e("RecordingAPI", "Subscribed to the Recording API");
+                    }
+                }
+            }
+        };
+
+        mCancelSubscriptionResultCallback = new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if (status.isSuccess()) {
+                    Log.e( "RecordingAPI", "Canceled subscriptions!");
+                } else {
+                    // Subscription not removed
+                    Log.e("RecordingAPI", "Failed to cancel subscriptions");
+                }
+            }
+        };
+
+        mListSubscriptionsResultCallback = new ResultCallback<ListSubscriptionsResult>() {
+            @Override
+            public void onResult(@NonNull ListSubscriptionsResult listSubscriptionsResult) {
+                for (Subscription subscription : listSubscriptionsResult.getSubscriptions()) {
+                    DataType dataType = subscription.getDataType();
+                    Log.e( "RecordingAPI", dataType.getName() );
+                    for (Field field : dataType.getFields() ) {
+                        Log.e( "RecordingAPI", field.toString() );
+                    }
+                }
+            }
+        };
+    }
+
+    //Ble Part
+    private void startBleScan () {
+        // 1. Define a callback object
+        BleScanCallback callback = new BleScanCallback() {
+            @Override
+            public void onDeviceFound(BleDevice device) {
+                // A device that provides the requested data types is available
+                // -> Claim this BLE device (See next example)
+                claimBleDevice(device);
+
+
+            }
+            @Override
+            public void onScanStopped() {
+                // The scan timed out or was interrupted
+                Log.e( "startBleScan", "onScanStopped" );
+            }
+        };
+
+        // 2. Create a scan request object:
+        // - Specify the data types you're interested in
+        // - Provide the callback object
+        StartBleScanRequest request = new StartBleScanRequest.Builder()
+                .setDataTypes(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+                .setBleScanCallback(callback)
+                .build();
+
+        // 3. Invoke the Bluetooth Low Energy API with:
+        // - The Google API client
+        // - The scan request
+        PendingResult<Status> pendingResult =
+                Fitness.BleApi.startBleScan(mApiClient, request);
+
+        // 4. Check the result (see other examples)
+        pendingResult.setResultCallback(mResultCallback);
+    }
+
+    //Ble Part
+    private void claimBleDevice (BleDevice bleDevice) {
+        // After the platform invokes your callback
+        // with a compatible BLE device (bleDevice):
+
+        // 1. Invoke the Bluetooth Low Energy API with:
+        // - The Google API client
+        // - The BleDevice object provided in the callback
+        PendingResult<Status> pendingResult =
+                Fitness.BleApi.claimBleDevice(mApiClient, bleDevice);
+
+        // 2. Check the result (see other examples)
+        pendingResult.setResultCallback(mResultCallback);
+    }
+
+    //Ble Part
+    private void unclaimBleDevice (BleDevice bleDevice) {
+        // When you no longer need the BLE device
+
+        // 1. Invoke the Bluetooth Low Energy API with:
+        // - The Google API client
+        // - The BLE device (from the initial scan)
+        PendingResult<Status> pendingResult =
+                Fitness.BleApi.unclaimBleDevice(mApiClient, bleDevice);
+
+        // 2. Check the result (see other examples)
+        pendingResult.setResultCallback(mResultCallback);
+    }
+}
